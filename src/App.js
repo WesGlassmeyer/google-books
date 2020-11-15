a7497528971c35cd7d7ec3e47dddcde85f1537c9
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2020-11-14 22:15:54 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from ".Header/Header";
+import SearchBar from "./SearchBar/SearchBar";
+import BookList from "./BookList/BookList";
+import Filter from "./Filter/Filter";
+
+export default class App extends Component {
+  state = {
+    bookResults: this.props.starterBookResults,
+    searchQuery: "the+art+of+war",
+    bookFilter: "",
+    printFilter: "",
+  };
+
+  handleSearchSubmit = (searchSubmitEvent, searchInput) => {
+    searchSubmitEvent.preventDefault();
+    this.setState({
+      searchQuery: searchInput,
+    });
+    const baseUrl = `https://www.googleapis.com/books/v1/volumes`;
+    const key = "AIzaSyBD8-kkEeKvgBdhiEK0DCNX6mia5EuWii0";
+    const formattedSearchUrl = this.formatQuery(baseUrl, searchInput, key);
+    fetch(formattedSearchUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong. Please try again later.");
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then((bookResultsObj) => {
+        console.log("Good response from Google Books API:", bookResultsObj);
+        this.setState({
+          bookResults: bookResultsObj,
+          error: null,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message,
+        });
+      });
+  };
+
+  formatQuery = (baseUrl, searchInput, key) => {
+    const { bookFilter, printFilter } = this.state;
+    let formattedQuery;
+    if (searchInput !== "") {
+      formattedQuery = "?q=" + searchInput;
+    }
+    if (bookFilter !== "") {
+      formattedQuery = formattedQuery + "&filter=" + bookFilter;
+    }
+    if (printFilter !== "") {
+      formattedQuery = formattedQuery + "&bookType=" + printFilter;
+    }
+    const formattedUrl = baseUrl + formattedQuery + "&key=" + key;
+    console.log("formatted URL:", formattedUrl);
+    return formattedUrl;
+  };
+
+  handlePrintType = (printTypeFormEvent) => {
+    if (printTypeFormEvent) {
+      this.setState({
+        printFilter: printTypeFormEvent,
+      });
+    }
+  };
+
+  handleBookType = (bookTypeFormEvent) => {
+    if (bookTypeFormEvent) {
+      this.setState({
+        bookFilter: bookTypeFormEvent,
+      });
+    }
+  };
+
+  render() {
+    const { bookResults } = this.state;
+    const isMobile = window.innerWidth <= 500;
+    let responsiveFilter;
+    if (isMobile) {
+      responsiveFilter = null;
+    } else {
+      responsiveFilter = (
+        <Filter
+          handlePrintType={this.handlePrintType}
+          handleBookType={this.handleBookType}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <SearchBar handleSearchSubmit={this.handleSearchSubmit} />
+        {responsiveFilter}
+        <BookList bookResults={bookResults} />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,425 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const starterLotResults = {
+  kind: "books#volumes",
+  totalItems: 1093,
+  items: [
+    {
+      kind: "books#volume",
+      id: "diaZ3REPJ4kC",
+      etag: "xMXGrayYpjU",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/diaZ3REPJ4kC",
+      volumeInfo: {
+        title: "Brazilian Jiu-jitsu",
+        subtitle: "Theory & Technique",
+        authors: ["Renzo Gracie", "Royler Gracie", "Kid Peligro"],
+        publishedDate: "2001",
+        description:
+          "Step-by-step photographs and illustrations demonstrate more than one hundred maneuvers from the traditional martial art of Brazilian jiu-jitsu, with an overview of the history of jiu-jitsu and its uses.",
+        industryIdentifiers: [
+          {
+            type: "ISBN_10",
+            identifier: "1931229082",
+          },
+          {
+            type: "ISBN_13",
+            identifier: "9781931229081",
+          },
+        ],
+        readingModes: {
+          text: false,
+          image: false,
+        },
+        pageCount: 255,
+        printType: "BOOK",
+        categories: ["Sports & Recreation"],
+        averageRating: 4,
+        ratingsCount: 3,
+        maturityRating: "NOT_MATURE",
+        allowAnonLogging: false,
+        contentVersion: "1.0.1.0.preview.0",
+        panelizationSummary: {
+          containsEpubBubbles: false,
+          containsImageBubbles: false,
+        },
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=diaZ3REPJ4kC&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=diaZ3REPJ4kC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        },
+        language: "en",
+        previewLink:
+          "http://books.google.com/books?id=diaZ3REPJ4kC&q=jiu+jitsu&dq=jiu+jitsu&hl=&cd=1&source=gbs_api",
+        infoLink:
+          "http://books.google.com/books?id=diaZ3REPJ4kC&dq=jiu+jitsu&hl=&source=gbs_api",
+        canonicalVolumeLink:
+          "https://books.google.com/books/about/Brazilian_Jiu_jitsu.html?hl=&id=diaZ3REPJ4kC",
+      },
+      saleInfo: {
+        country: "US",
+        saleability: "NOT_FOR_SALE",
+        isEbook: false,
+      },
+      accessInfo: {
+        country: "US",
+        viewability: "NO_PAGES",
+        embeddable: false,
+        publicDomain: false,
+        textToSpeechPermission: "ALLOWED",
+        epub: {
+          isAvailable: false,
+        },
+        pdf: {
+          isAvailable: false,
+        },
+        webReaderLink:
+          "http://play.google.com/books/reader?id=diaZ3REPJ4kC&hl=&printsec=frontcover&source=gbs_api",
+        accessViewStatus: "NONE",
+        quoteSharingAllowed: false,
+      },
+      searchInfo: {
+        textSnippet:
+          "Step-by-step photographs and illustrations demonstrate more than one hundred maneuvers from the traditional martial art of Brazilian jiu-jitsu, with an overview of the history of jiu-jitsu and its uses.",
+      },
+    },
+    {
+      kind: "books#volume",
+      id: "xL2ROQAACAAJ",
+      etag: "EaFNYYbj9yM",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/xL2ROQAACAAJ",
+      volumeInfo: {
+        title: "Jiu-Jitsu University",
+        authors: ["Saulo Ribeiro", "Kevin Howell"],
+        publisher: "Victory Belt Publishing",
+        publishedDate: "2008-11-17",
+        description:
+          "Saulo Ribeiro—six-time Brazilian Jiu-Jitsu World Champion—is world-renowned for his functional jiu-jitsu knowledge and flawless technique. In Jiu-Jitsu University, Ribeiro shares with the public for the first time his revolutionary system of grappling, mapping out more than 200 techniques that carry you from white to black belt. Illuminating common jiu-jitsu errors and then illustrating practical remedies, this book is a must for all who train in jiu-jitsu. Not your run-of-the-mill technique book, Jiu-Jitsu University is a detailed training manual that will ultimately change the way jiu-jitsu is taught around the globe.",
+        industryIdentifiers: [
+          {
+            type: "ISBN_10",
+            identifier: "0981504434",
+          },
+          {
+            type: "ISBN_13",
+            identifier: "9780981504438",
+          },
+        ],
+        readingModes: {
+          text: false,
+          image: false,
+        },
+        pageCount: 368,
+        printType: "BOOK",
+        categories: ["Sports & Recreation"],
+        averageRating: 5,
+        ratingsCount: 1,
+        maturityRating: "NOT_MATURE",
+        allowAnonLogging: false,
+        contentVersion: "preview-1.0.0",
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=xL2ROQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=xL2ROQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        },
+        language: "en",
+        previewLink:
+          "http://books.google.com/books?id=xL2ROQAACAAJ&dq=jiu+jitsu&hl=&cd=2&source=gbs_api",
+        infoLink:
+          "http://books.google.com/books?id=xL2ROQAACAAJ&dq=jiu+jitsu&hl=&source=gbs_api",
+        canonicalVolumeLink:
+          "https://books.google.com/books/about/Jiu_Jitsu_University.html?hl=&id=xL2ROQAACAAJ",
+      },
+      saleInfo: {
+        country: "US",
+        saleability: "NOT_FOR_SALE",
+        isEbook: false,
+      },
+      accessInfo: {
+        country: "US",
+        viewability: "NO_PAGES",
+        embeddable: false,
+        publicDomain: false,
+        textToSpeechPermission: "ALLOWED",
+        epub: {
+          isAvailable: false,
+        },
+        pdf: {
+          isAvailable: false,
+        },
+        webReaderLink:
+          "http://play.google.com/books/reader?id=xL2ROQAACAAJ&hl=&printsec=frontcover&source=gbs_api",
+        accessViewStatus: "NONE",
+        quoteSharingAllowed: false,
+      },
+      searchInfo: {
+        textSnippet:
+          "Illuminating common jiu-jitsu errors and then illustrating practical remedies, this book is a must for all who train in jiu-jitsu.",
+      },
+    },
+    {
+      kind: "books#volume",
+      id: "JBkAAAAAYAAJ",
+      etag: "sMsi0OBlPaQ",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/JBkAAAAAYAAJ",
+      volumeInfo: {
+        title: "Jiu-jitsu",
+        subtitle:
+          "A Comprehensive and Copiously Illustrated Treatise on the Wonderful Japanese Method of Attack and Self-defense",
+        authors: ["Harry Hall Skinner"],
+        publishedDate: "1904",
+        industryIdentifiers: [
+          {
+            type: "OTHER",
+            identifier: "HARVARD:32044023475411",
+          },
+        ],
+        readingModes: {
+          text: false,
+          image: true,
+        },
+        pageCount: 118,
+        printType: "BOOK",
+        categories: ["Jiu-jitsu"],
+        maturityRating: "NOT_MATURE",
+        allowAnonLogging: false,
+        contentVersion: "0.2.3.0.full.1",
+        panelizationSummary: {
+          containsEpubBubbles: false,
+          containsImageBubbles: false,
+        },
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=JBkAAAAAYAAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=JBkAAAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        },
+        language: "en",
+        previewLink:
+          "http://books.google.com/books?id=JBkAAAAAYAAJ&printsec=frontcover&dq=jiu+jitsu&hl=&cd=3&source=gbs_api",
+        infoLink:
+          "https://play.google.com/store/books/details?id=JBkAAAAAYAAJ&source=gbs_api",
+        canonicalVolumeLink:
+          "https://play.google.com/store/books/details?id=JBkAAAAAYAAJ",
+      },
+      saleInfo: {
+        country: "US",
+        saleability: "FREE",
+        isEbook: true,
+        buyLink:
+          "https://play.google.com/store/books/details?id=JBkAAAAAYAAJ&rdid=book-JBkAAAAAYAAJ&rdot=1&source=gbs_api",
+      },
+      accessInfo: {
+        country: "US",
+        viewability: "ALL_PAGES",
+        embeddable: true,
+        publicDomain: true,
+        textToSpeechPermission: "ALLOWED",
+        epub: {
+          isAvailable: false,
+          downloadLink:
+            "http://books.google.com/books/download/Jiu_jitsu.epub?id=JBkAAAAAYAAJ&hl=&output=epub&source=gbs_api",
+        },
+        pdf: {
+          isAvailable: true,
+          downloadLink:
+            "http://books.google.com/books/download/Jiu_jitsu.pdf?id=JBkAAAAAYAAJ&hl=&output=pdf&sig=ACfU3U2OjEOegAqzL1fdpkRu6bVDJV-O-w&source=gbs_api",
+        },
+        webReaderLink:
+          "http://play.google.com/books/reader?id=JBkAAAAAYAAJ&hl=&printsec=frontcover&source=gbs_api",
+        accessViewStatus: "FULL_PUBLIC_DOMAIN",
+        quoteSharingAllowed: false,
+      },
+    },
+    {
+      kind: "books#volume",
+      id: "rzBHPQAACAAJ",
+      etag: "uS8sof1XKJg",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/rzBHPQAACAAJ",
+      volumeInfo: {
+        title: "Guerrilla Jiu-Jitsu",
+        subtitle: "Revolutionizing Brazilian Jiu-jitsu",
+        authors: ["Dave Camarillo", "Erich Krauss", "Eric Hendrikx"],
+        publisher: "Victory Belt Publishing",
+        publishedDate: "2006-10-15",
+        description:
+          "Guerrilla Jiu-Jitsu is a groundbreaking system of grappling that integrates the Olympic sport of judo with Brazilian Jiu-Jitsu, today's most sought after grappling art. These are the techniques and strategies that allowed Camarillo to devastate the competition at some of the world's most prestigious judo and jiu-jitsu tournaments and have led his students to more than a dozen victories in the Ultimate Fighting Championship. Guerrilla Jiu-Jitsu offers a leg up on virtually every opponent who refuses to stray from the parameters of his discipline.",
+        industryIdentifiers: [
+          {
+            type: "ISBN_10",
+            identifier: "0977731588",
+          },
+          {
+            type: "ISBN_13",
+            identifier: "9780977731589",
+          },
+        ],
+        readingModes: {
+          text: false,
+          image: false,
+        },
+        pageCount: 228,
+        printType: "BOOK",
+        categories: ["Sports & Recreation"],
+        maturityRating: "NOT_MATURE",
+        allowAnonLogging: false,
+        contentVersion: "preview-1.0.0",
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=rzBHPQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=rzBHPQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        },
+        language: "en",
+        previewLink:
+          "http://books.google.com/books?id=rzBHPQAACAAJ&dq=jiu+jitsu&hl=&cd=4&source=gbs_api",
+        infoLink:
+          "http://books.google.com/books?id=rzBHPQAACAAJ&dq=jiu+jitsu&hl=&source=gbs_api",
+        canonicalVolumeLink:
+          "https://books.google.com/books/about/Guerrilla_Jiu_Jitsu.html?hl=&id=rzBHPQAACAAJ",
+      },
+      saleInfo: {
+        country: "US",
+        saleability: "NOT_FOR_SALE",
+        isEbook: false,
+      },
+      accessInfo: {
+        country: "US",
+        viewability: "NO_PAGES",
+        embeddable: false,
+        publicDomain: false,
+        textToSpeechPermission: "ALLOWED",
+        epub: {
+          isAvailable: false,
+        },
+        pdf: {
+          isAvailable: false,
+        },
+        webReaderLink:
+          "http://play.google.com/books/reader?id=rzBHPQAACAAJ&hl=&printsec=frontcover&source=gbs_api",
+        accessViewStatus: "NONE",
+        quoteSharingAllowed: false,
+      },
+      searchInfo: {
+        textSnippet:
+          "These are the techniques and strategies that allowed Camarillo to devastate the competition at some of the world&#39;s most prestigious judo and jiu-jitsu tournaments and have led his students to more than a dozen victories in the Ultimate ...",
+      },
+    },
+    {
+      kind: "books#volume",
+      id: "Tg_QAgAAQBAJ",
+      etag: "VvD2OAyhc+0",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/Tg_QAgAAQBAJ",
+      volumeInfo: {
+        title: "Brazilian Jiu-Jitsu",
+        subtitle:
+          "The Ultimate Guide to Dominating Brazilian Jiu-Jitsu and Mixed Martial Arts Combat",
+        authors: ["Alexandre Paiva"],
+        publisher: "Tuttle Publishing",
+        publishedDate: "2013-02-12",
+        description:
+          "Brazilian Jiu-Jitsu is a guide to the most effective and devastating techniques in popular martial arts by World Champion and Brazilian Jiu Jitsu legend Alexandre Paiva. The book contains over 1,000 full-color photographs demonstrating the moves that made the author the most feared competitor in BJJ circles and now one of the most sought-after instructors. Jiu-Jitsu or Jujutsu came into prominence in the early 90's when jiu-jitsu expert Royce Gracie won the first, second, and fourth Ultimate Fighting Championships against much larger opponents who were using other styles such as boxing, muay thai, karate, and wrestling. Since then it has become one of the most popular styles in MMA due to its focus on ground fighting. Learn the techniques that have proven dominant in Brazilian Jiu-Jitsu tournaments and in the Mixed Martial Arts cage: Takedowns The Closed Guard Passing The Closed Guard Butterfly Guard Open Guard Pass Side Control Half Guard Back Mount With sections devoted to both gi and no-gi techniques, whether training for a Brazilian Jiu-Jitsu tournament or a Mixed Martial Arts fight, Brazilian Jiu-Jitsu is one book no true competitor can afford to miss.",
+        industryIdentifiers: [
+          {
+            type: "ISBN_13",
+            identifier: "9781462910007",
+          },
+          {
+            type: "ISBN_10",
+            identifier: "1462910009",
+          },
+        ],
+        readingModes: {
+          text: true,
+          image: true,
+        },
+        pageCount: 192,
+        printType: "BOOK",
+        categories: ["Sports & Recreation"],
+        maturityRating: "NOT_MATURE",
+        allowAnonLogging: true,
+        contentVersion: "1.20.21.0.preview.3",
+        panelizationSummary: {
+          containsEpubBubbles: false,
+          containsImageBubbles: false,
+        },
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=Tg_QAgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          thumbnail:
+            "http://books.google.com/books/content?id=Tg_QAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        },
+        language: "en",
+        previewLink:
+          "http://books.google.com/books?id=Tg_QAgAAQBAJ&printsec=frontcover&dq=jiu+jitsu&hl=&cd=5&source=gbs_api",
+        infoLink:
+          "https://play.google.com/store/books/details?id=Tg_QAgAAQBAJ&source=gbs_api",
+        canonicalVolumeLink:
+          "https://play.google.com/store/books/details?id=Tg_QAgAAQBAJ",
+      },
+      saleInfo: {
+        country: "US",
+        saleability: "FOR_SALE",
+        isEbook: true,
+        listPrice: {
+          amount: 24.95,
+          currencyCode: "USD",
+        },
+        retailPrice: {
+          amount: 13.99,
+          currencyCode: "USD",
+        },
+        buyLink:
+          "https://play.google.com/store/books/details?id=Tg_QAgAAQBAJ&rdid=book-Tg_QAgAAQBAJ&rdot=1&source=gbs_api",
+        offers: [
+          {
+            finskyOfferType: 1,
+            listPrice: {
+              amountInMicros: 24950000,
+              currencyCode: "USD",
+            },
+            retailPrice: {
+              amountInMicros: 13990000,
+              currencyCode: "USD",
+            },
+            giftable: true,
+          },
+        ],
+      },
+      accessInfo: {
+        country: "US",
+        viewability: "PARTIAL",
+        embeddable: true,
+        publicDomain: false,
+        textToSpeechPermission: "ALLOWED",
+        epub: {
+          isAvailable: true,
+          acsTokenLink:
+            "http://books.google.com/books/download/Brazilian_Jiu_Jitsu-sample-epub.acsm?id=Tg_QAgAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api",
+        },
+        pdf: {
+          isAvailable: true,
+          acsTokenLink:
+            "http://books.google.com/books/download/Brazilian_Jiu_Jitsu-sample-pdf.acsm?id=Tg_QAgAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api",
+        },
+        webReaderLink:
+          "http://play.google.com/books/reader?id=Tg_QAgAAQBAJ&hl=&printsec=frontcover&source=gbs_api",
+        accessViewStatus: "SAMPLE",
+        quoteSharingAllowed: false,
+      },
+      searchInfo: {
+        textSnippet:
+          "The book contains over 1,000 full-color photographs demonstrating the moves that made the author the most feared competitor in BJJ circles and now one of the most sought-after instructors.",
+      },
+    },
+  ],
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App starterBookResults={starterLotResults} />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
