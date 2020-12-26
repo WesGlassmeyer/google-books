423bf960bcc50bd8fce8bedd3701544b84dd3f1a
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-12-25 17:04:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
       searchQuery: searchInput,
     });
     const baseUrl = `https://www.googleapis.com/books/v1/volumes`;
-    const key = "AIzaSyBD8-kkEeKvgBdhiEK0DCNX6mia5EuWii0";
+    const key = "";
     const formattedSearchUrl = this.formatQuery(baseUrl, searchInput, key);
     fetch(formattedSearchUrl)
       .then((response) => {
